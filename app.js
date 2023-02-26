e3eb575a7aca47ec21ea379813a5c5733b690c92
Detravoir <thomasdiondehaas@hotmail.com>
2023-02-26 17:16:57 +0100

    loading works

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const videoStatus = document.getElementById("videoStatus");
 const labelOneBtn = document.querySelector("#labelOne");
 const labelTwoBtn = document.querySelector("#labelTwo");
 const labelThreeBtn = document.querySelector("#labelThree");
+
 const amountOne = document.getElementById("amountOne");
 const amountTwo = document.getElementById("amountTwo");
 const amountThree = document.getElementById("amountThree");
@@ -34,63 +35,64 @@ if (navigator.mediaDevices.getUserMedia) {
 
 label.innerText = "Ready when you are!";
 
+const featureExtractor = ml5.featureExtractor("MobileNet", modelLoaded);
+
+const classifier = featureExtractor.classification(video, videoReady);
+
 function modelLoaded() {
-    loading.innerText = "Model loaded!";
-  }
-  
-  const featureExtractor = ml5.featureExtractor("MobileNet", modelLoaded);
+  loading.innerText = "Model loaded!";
+  classifier.load('model.json');
+}
 
-  const classifier = featureExtractor.classification(video, videoReady);
-
-  // A function to be called when the video is finished loading
+// A function to be called when the video is finished loading
 function videoReady() {
-    videoStatus.innerText = "Video ready!";
-  }
+  videoStatus.innerText = "Video ready!";
+}
 
-  labelOneBtn.onclick = function() {
-    classifier.addImage("one");
-    amountOne.innerText = Number(amountOne.innerText) + 1;
-  };
+labelOneBtn.onclick = function() {
+  classifier.addImage("raccoon");
+  amountOne.innerText = Number(amountOne.innerText) + 1;
+};
 
-  labelTwoBtn.onclick = function() {
-    classifier.addImage("two");
-    amountTwo.innerText = Number(amountTwo.innerText) + 1;
-  };
+labelTwoBtn.onclick = function() {
+  classifier.addImage("cat");
+  amountTwo.innerText = Number(amountTwo.innerText) + 1;
+};
 
-  labelThreeBtn.onclick = function() {
-    classifier.addImage("three");
-    amountThree.innerText = Number(amountThree.innerText) + 1;
-  };
+labelThreeBtn.onclick = function() {
+  classifier.addImage("dog");
+  amountThree.innerText = Number(amountThree.innerText) + 1;
+};
 
-  train.onclick = function() {
-    classifier.train(function(lossValue) {
-      if (lossValue) {
-        totalLoss = lossValue;
-        loss.innerHTML = `Loss: ${totalLoss}`;
-      } else {
-        loss.innerHTML = `Done Training! Final Loss: ${totalLoss}`;
-      }
-    });
-  };
-  
-  // Show the results
-  function gotResults(err, results) {
-    // Display any error
-    if (err) {
-      console.error(err);
+train.onclick = function() {
+  classifier.train(function(lossValue) {
+    if (lossValue) {
+      totalLoss = lossValue;
+      loss.innerHTML = `Loss: ${totalLoss}`;
+    } else {
+      loss.innerHTML = `Done Training! Final Loss: ${totalLoss}`;
     }
-    if (results && results[0]) {
-      result.innerText = results[0].label;
-      confidence.innerText = results[0].confidence;
-      classifier.classify(gotResults);
-    }
-  }
+  });
+};
   
-  save.onclick = function() {
-    featureExtractor.save();
-  };
-
-  // Start predicting when the predict button is clicked
-  predict.onclick = function() {
+// Show the results
+function gotResults(err, modelLoaded) {
+  // Display any error
+  if (err) {
+    console.error(err);
+  }
+  if (modelLoaded && modelLoaded[0]) {
+    result.innerText = modelLoaded[0].label;
+    confidence.innerText = modelLoaded[0].confidence;
     classifier.classify(gotResults);
-  };
+  }
+}
+  
+save.onclick = function() {
+   featureExtractor.save();
+};
+
+// Start predicting when the predict button is clicked
+predict.onclick = function() {
+  classifier.classify(gotResults);
+};
