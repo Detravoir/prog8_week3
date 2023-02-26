820996091d22d2d2b31ccc0fdc015111d0fcf719
Detravoir <thomasdiondehaas@hotmail.com>
2023-02-26 11:49:16 +0100

    plus week 3 code

--- app.js
+++ app.js
@@ -11,6 +11,32 @@ const result = document.getElementById("result");
 const confidence = document.getElementById("confidence");
 const predict = document.getElementById("predict");
 
+const label = document.getElementById("label");
+
+const labelOneBtn = document.querySelector("#labelOne");
+const labelTwoBtn = document.querySelector("#labelTwo");
+const labelThreeBtn = document.querySelector("#labelThree");
+const trainbtn = document.querySelector("#train");
+
+labelOneBtn.addEventListener("click", () => console.log("button 1"));
+labelTwoBtn.addEventListener("click", () => console.log("button 2"));
+labelThreeBtn.addEventListener("click", () => console.log("button 3"));
+
+trainbtn.addEventListener("click", () => console.log("train"));
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+          video.srcObject = stream;
+      })
+      .catch((err) => {
+          console.log("Something went wrong!");
+      });
+}
+
+label.innerText = "Ready when you are!";
+
 // A variable to store the total loss
 let totalLoss = 0;
 
