696d3e26c4739a678ef6a4da73caf33ab16bd751
Detravoir <thomasdiondehaas@hotmail.com>
2023-02-26 20:35:29 +0100

    fixed for mobile

--- app.js
+++ app.js
@@ -1,139 +1,60 @@
-const video = document.getElementById("webcam");
 const label = document.getElementById("label");
-const videoStatus = document.getElementById("videoStatus");
-const labelOneBtn = document.querySelector("#labelOne");
-const labelTwoBtn = document.querySelector("#labelTwo");
-const labelThreeBtn = document.querySelector("#labelThree");
-
-const amountOne = document.getElementById("amountOne");
-const amountTwo = document.getElementById("amountTwo");
-const amountThree = document.getElementById("amountThree");
-const trainbtn = document.querySelector("#train");
-const loss = document.getElementById("loss");
-const result = document.getElementById("result");
-const confidence = document.getElementById("confidence");
-const predict = document.getElementById("predict");
-const save = document.getElementById("save");
-
-let totalLoss = 0;
-labelOneBtn.addEventListener("click", () => console.log("button 1"));
-labelTwoBtn.addEventListener("click", () => console.log("button 2"));
-labelThreeBtn.addEventListener("click", () => console.log("button 3"));
-
-trainbtn.addEventListener("click", () => console.log("train"));
-
-// if (navigator.mediaDevices.getUserMedia) {
-//     navigator.mediaDevices
-//         .getUserMedia({ video: true })
-//         .then((stream) => {
-//             video.srcObject = stream;
-//         })
-//         .catch((err) => {
-//             console.log("Something went wrong!");
-//         });
-// }
-
-label.innerText = "Ready when you are!";
-
-const featureExtractor = ml5.featureExtractor("MobileNet", modelLoaded);
-
-//const classifier = featureExtractor.classification(video, videoReady);
-
-let classifier = ml5.imageClassifier('MobileNet');
-let img;
-
-const image_input = document.querySelector('#image_input');
-let uploaded_image = "";
-
-image_input.addEventListener('change', function(){
-  const reader = new FileReader();
-  reader.addEventListener("load", () => {
-    uploaded_image = reader.result;
-    document.querySelector('#display_image').style.backgroundImage = `url(${uploaded_image})`;
-    
-  });
-  reader.readAsDataURL(this.files[0]);
-  
-})
-
-
- function imageReady(){
-   image(img, 0, 0, width, height)
- }
-
-function setup() {
-  img = createImg('cat3.jpg', imageReady);
-}
+const loading = document.getElementById("loading");
+const classifier = ml5.imageClassifier('model.json', modelLoaded);
 
 function modelLoaded() {
   loading.innerText = "Model loaded!";
-  classifier = ml5.imageClassifier('model.json');
-  classifier.classify(img, gotResult);
 }
 
-function gotResult(error, modelLoaded) {
-  // Display error in the console
-  if (error) {
-    console.error(error);
+let synth = window.speechSynthesis;
+
+function speak(text){
+  if (synth.speaking){
+    console.log('still speaking');
+    return;
   }
-  //The results are in an array ordered by confidence.
-  console.log(modelLoaded);
-  createDiv(`Label: ${modelLoaded[0].label}`);
-  createDiv(`Confidence: ${nf(modelLoaded[0].confidence, 0, 2)}`);
+  if (text !== ''){
+    let utterThis = new SpeechSynthesisUtterance(text);
+    synth.speak(utterThis);
+  }
 }
 
-// A function to be called when the video is finished loading
-// function videoReady() {
-//   videoStatus.innerText = "Video ready!";
-// }
+// Listen for when the user selects an image
+const inputElement = document.getElementById("image");
+inputElement.addEventListener("change", (e) => {
+  const imageFile = e.target.files[0];
 
-// labelOneBtn.onclick = function() {
-//   classifier.addImage("raccoon");
-//   amountOne.innerText = Number(amountOne.innerText) + 1;
-// };
+  // Remove the previously uploaded image element
+  const imageContainer = document.getElementById("image-container");
+  const lastUploadedImage = imageContainer.lastElementChild;
+  if (lastUploadedImage) {
+    imageContainer.removeChild(lastUploadedImage);
+  }
 
-// labelTwoBtn.onclick = function() {
-//   classifier.addImage("cat");
-//   amountTwo.innerText = Number(amountTwo.innerText) + 1;
-// };
+  // Create a new image element and set the source to the selected file
+  const img = new Image();
+  img.src = URL.createObjectURL(imageFile);
 
-// labelThreeBtn.onclick = function() {
-//   classifier.addImage("dog");
-//   amountThree.innerText = Number(amountThree.innerText) + 1;
-// };
-
-// train.onclick = function() {
-//   classifier.train(function(lossValue) {
-//     if (lossValue) {
-//       totalLoss = lossValue;
-//       loss.innerHTML = `Loss: ${totalLoss}`;
-//     } else {
-//       loss.innerHTML = `Done Training! Final Loss: ${totalLoss}`;
-//     }
-//   });
-// };
-  
-// // Show the results
-// function gotResult(err, modelLoaded) {
-//   // Display any error
-//   if (err) {
-//     console.error(err);
-//   }
-//   if (modelLoaded && modelLoaded[0]) {
-//     result.innerText = modelLoaded[0].label;
-//     confidence.innerText = modelLoaded[0].confidence;
-//     classifier.classify(gotResult);
-//   }
-//}
-
-  
-// save.onclick = function() {
-//    featureExtractor.save();
-// }
-
-//Start predicting when the predict button is clicked
-// predict.onclick = function() {
-//   classifier.classify(gotResults);
-// };
+  // When the image is loaded, classify the image and update the label
+  img.onload = () => {
+    classifier.classify(img, (err, results) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      label.innerText = `Prediction: ${results[0].label} (${results[0].confidence.toFixed(2)})`;
+      if (label.innerText.includes("raccoon")){
+        speak("raccoon");
+      } else if(label.innerText.includes("cat")){
+        speak("cat");
+      }
+    });
+  };
 
 
+
+  // Add the image element to the DOM
+  const uploadedImage = document.createElement("img");
+  uploadedImage.src = URL.createObjectURL(imageFile);
+  imageContainer.appendChild(uploadedImage);
+});
