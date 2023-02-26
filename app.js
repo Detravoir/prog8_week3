766f11c1aebdd3340615a05a48dcd8b125f4f418
Detravoir <thomasdiondehaas@hotmail.com>
2023-02-26 19:51:39 +0100

    base

--- app.js
+++ app.js
@@ -22,77 +22,118 @@ labelThreeBtn.addEventListener("click", () => console.log("button 3"));
 
 trainbtn.addEventListener("click", () => console.log("train"));
 
-if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-            video.srcObject = stream;
-        })
-        .catch((err) => {
-            console.log("Something went wrong!");
-        });
-}
+// if (navigator.mediaDevices.getUserMedia) {
+//     navigator.mediaDevices
+//         .getUserMedia({ video: true })
+//         .then((stream) => {
+//             video.srcObject = stream;
+//         })
+//         .catch((err) => {
+//             console.log("Something went wrong!");
+//         });
+// }
 
 label.innerText = "Ready when you are!";
 
 const featureExtractor = ml5.featureExtractor("MobileNet", modelLoaded);
 
-const classifier = featureExtractor.classification(video, videoReady);
+//const classifier = featureExtractor.classification(video, videoReady);
+
+let classifier = ml5.imageClassifier('MobileNet');
+let img;
+
+const image_input = document.querySelector('#image_input');
+let uploaded_image = "";
+
+image_input.addEventListener('change', function(){
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    uploaded_image = reader.result;
+    document.querySelector('#display_image').style.backgroundImage = `url(${uploaded_image})`;
+    
+  });
+  reader.readAsDataURL(this.files[0]);
+  
+})
+
+
+ function imageReady(){
+   image(img, 0, 0, width, height)
+ }
+
+function setup() {
+  img = createImg('cat3.jpg', imageReady);
+}
 
 function modelLoaded() {
   loading.innerText = "Model loaded!";
-  classifier.load('model.json');
+  classifier = ml5.imageClassifier('model.json');
+  classifier.classify(img, gotResult);
+}
+
+function gotResult(error, modelLoaded) {
+  // Display error in the console
+  if (error) {
+    console.error(error);
+  }
+  //The results are in an array ordered by confidence.
+  console.log(modelLoaded);
+  createDiv(`Label: ${modelLoaded[0].label}`);
+  createDiv(`Confidence: ${nf(modelLoaded[0].confidence, 0, 2)}`);
 }
 
 // A function to be called when the video is finished loading
-function videoReady() {
-  videoStatus.innerText = "Video ready!";
-}
+// function videoReady() {
+//   videoStatus.innerText = "Video ready!";
+// }
 
-labelOneBtn.onclick = function() {
-  classifier.addImage("raccoon");
-  amountOne.innerText = Number(amountOne.innerText) + 1;
-};
+// labelOneBtn.onclick = function() {
+//   classifier.addImage("raccoon");
+//   amountOne.innerText = Number(amountOne.innerText) + 1;
+// };
 
-labelTwoBtn.onclick = function() {
-  classifier.addImage("cat");
-  amountTwo.innerText = Number(amountTwo.innerText) + 1;
-};
+// labelTwoBtn.onclick = function() {
+//   classifier.addImage("cat");
+//   amountTwo.innerText = Number(amountTwo.innerText) + 1;
+// };
 
-labelThreeBtn.onclick = function() {
-  classifier.addImage("dog");
-  amountThree.innerText = Number(amountThree.innerText) + 1;
-};
+// labelThreeBtn.onclick = function() {
+//   classifier.addImage("dog");
+//   amountThree.innerText = Number(amountThree.innerText) + 1;
+// };
 
-train.onclick = function() {
-  classifier.train(function(lossValue) {
-    if (lossValue) {
-      totalLoss = lossValue;
-      loss.innerHTML = `Loss: ${totalLoss}`;
-    } else {
-      loss.innerHTML = `Done Training! Final Loss: ${totalLoss}`;
-    }
-  });
-};
+// train.onclick = function() {
+//   classifier.train(function(lossValue) {
+//     if (lossValue) {
+//       totalLoss = lossValue;
+//       loss.innerHTML = `Loss: ${totalLoss}`;
+//     } else {
+//       loss.innerHTML = `Done Training! Final Loss: ${totalLoss}`;
+//     }
+//   });
+// };
   
-// Show the results
-function gotResults(err, modelLoaded) {
-  // Display any error
-  if (err) {
-    console.error(err);
-  }
-  if (modelLoaded && modelLoaded[0]) {
-    result.innerText = modelLoaded[0].label;
-    confidence.innerText = modelLoaded[0].confidence;
-    classifier.classify(gotResults);
-  }
-}
-  
-save.onclick = function() {
-   featureExtractor.save();
-};
+// // Show the results
+// function gotResult(err, modelLoaded) {
+//   // Display any error
+//   if (err) {
+//     console.error(err);
+//   }
+//   if (modelLoaded && modelLoaded[0]) {
+//     result.innerText = modelLoaded[0].label;
+//     confidence.innerText = modelLoaded[0].confidence;
+//     classifier.classify(gotResult);
+//   }
+//}
 
-// Start predicting when the predict button is clicked
-predict.onclick = function() {
-  classifier.classify(gotResults);
-};
+  
+// save.onclick = function() {
+//    featureExtractor.save();
+// }
+
+//Start predicting when the predict button is clicked
+// predict.onclick = function() {
+//   classifier.classify(gotResults);
+// };
+
+
